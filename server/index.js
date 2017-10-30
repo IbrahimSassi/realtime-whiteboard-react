6c4f9f680ab2fa0cd6e506e695c2f484651149b1
ibrahim <ibrahim.sassi@esprit.tn>
2017-10-30 22:02:27 +0100

    connecting to rethink db

--- server/index.js
+++ server/index.js
@@ -1,15 +1,33 @@
 const io = require('socket.io')();
 const port = 8000;
+const r = require('rethinkdb');
 
 
-io.on('connection', (client) => {
-    client.on('subscribeToTimer', (interval) => {
-        console.log("client is subscribing to timer with ", interval);
-        setInterval(() => {
-            client.emit('timer', new Date());
-        }, interval)
+r.connect({
+    host: 'localhost',
+    port: 28015,
+    db: 'awesome_whiteboard'
+}).then((connection) => {
+
+
+    io.on('connection', (client) => {
+        client.on('subscribeToTimer', (interval) => {
+            console.log("client is subscribing to timer with ", interval);
+            r.table('timers')
+                .changes()
+                .run(connection)
+                .then((cursor) => {
+                    cursor.each((err, timerRow) => {
+                        console.log(timerRow);
+                        client.emit('timer', timerRow.new_val.timestamp);
+                    })
+                })
+        })
     })
+
+
 })
+
 
 io.listen(8000);
 
